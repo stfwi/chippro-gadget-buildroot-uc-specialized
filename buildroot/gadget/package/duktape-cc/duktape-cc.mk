590e88b79904d513d33088e5d1d238daa7b566a9
stfwi <cerberos@atwillys.de>
2017-10-30 22:23:26 +0100

    ECMA interpreter binary adapted.

--- buildroot/gadget/package/duktape-cc/duktape-cc.mk
+++ buildroot/gadget/package/duktape-cc/duktape-cc.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# duktape-cc CLI binary (/usr/bin/js)
+# duktape-cc CLI binary (/usr/bin/djs)
 #
 ################################################################################
 
@@ -11,17 +11,17 @@ DUKTAPE_CC_LICENSE = MIT
 #####
 
 define DUKTAPE_CC_BUILD_CMDS
-	rm -f $(TARGET_DIR)/usr/bin/js
-	rm -f $(STAGING_DIR)/usr/bin/js
+	rm -f $(TARGET_DIR)/usr/bin/djs
+	rm -f $(STAGING_DIR)/usr/bin/djs
 	$(MAKE) -C $(@D) binary CXX=$(TARGET_CXX) STRIP=$(TARGET_STRIP) FLAGS=$(CXXFLAGS)
 endef
 
 define DUKTAPE_CC_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/cli/js $(STAGING_DIR)/usr/bin/js
+	$(INSTALL) -D -m 0755 $(@D)/cli/djs $(STAGING_DIR)/usr/bin/djs
 endef
 
 define DUKTAPE_CC_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/cli/js $(TARGET_DIR)/usr/bin/js
+	$(INSTALL) -D -m 0755 $(@D)/cli/djs $(TARGET_DIR)/usr/bin/djs
 endef
 
 $(eval $(generic-package))
